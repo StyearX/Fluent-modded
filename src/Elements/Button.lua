7b75c03733164610d66f0383e2b8bc024604d743
StyearX <nazzz9442@gmail.com>
2026-06-05 10:02:22 +0800

    update Elements source

--- src/Elements/Button.lua
+++ src/Elements/Button.lua
@@ -1,54 +1,36 @@
-local Root    = script.Parent.Parent
+local Root = script.Parent.Parent
 local Creator = require(Root.Creator)
-local New     = Creator.New
 
-local Button = {}
-Button.__index = Button
-Button.__type  = "Button"
+local New = Creator.New
+local Components = Root.Components
 
-function Button:New(_, config)
-	assert(config.Title, "Button - Missing Title")
-	config.Callback = config.Callback or function() end
-	local lib = self.Library
+local Element = {}
+Element.__index = Element
+Element.__type = "Button"
 
-	local el = require(Root.Components.Element)(config.Title, config.Description, self.Container, true)
+function Element:New(Config)
+	assert(Config.Title, "Button - Missing Title")
+	Config.Callback = Config.Callback or function() end
 
-	local iconData = config.Icon and lib:GetIcon(config.Icon)
-	local imgId    = "rbxassetid://10709791437"
-	local rectOff  = Vector2.new()
-	local rectSz   = Vector2.new()
-	if iconData then
-		if type(iconData) == "table" then
-			imgId   = iconData.Image           or imgId
-			rectOff = iconData.ImageRectOffset or rectOff
-			rectSz  = iconData.ImageRectSize   or rectSz
-		else
-			imgId = tostring(iconData)
-		end
-	end
+	local ButtonFrame = require(Components.Element)(Config.Title, Config.Description, self.Container, true)
 
-	New("ImageLabel", {
-		Image                = imgId,
-		ImageRectOffset      = rectOff,
-		ImageRectSize        = rectSz,
-		Size                 = UDim2.fromOffset(16, 16),
-		AnchorPoint          = Vector2.new(1, 0.5),
-		Position             = UDim2.new(1, -10, 0.5, 0),
+	local ButtonIco = New("ImageLabel", {
+		Image = "rbxassetid://10709791437",
+		Size = UDim2.fromOffset(16, 16),
+		AnchorPoint = Vector2.new(1, 0.5),
+		Position = UDim2.new(1, -10, 0.5, 0),
 		BackgroundTransparency = 1,
-		Parent               = el.Frame,
-		ThemeTag             = { ImageColor3 = "Text" },
+		Parent = ButtonFrame.Frame,
+		ThemeTag = {
+			ImageColor3 = "Text",
+		},
 	})
 
-	Creator.AddSignal(el.Frame.MouseButton1Click, function()
-		lib:SafeCallback(config.Callback)
+	Creator.AddSignal(ButtonFrame.Frame.MouseButton1Click, function()
+		self.Library:SafeCallback(Config.Callback)
 	end)
 
-	function el:SetDesc(text)
-		el.DescLabel.Text    = text or ""
-		el.DescLabel.Visible = text ~= nil and text ~= ""
-	end
-
-	return el
+	return ButtonFrame
 end
 
-return Button
+return Element
